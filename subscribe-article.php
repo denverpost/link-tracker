7415a4cc8d81c4c12830353a19872c51648eb591
Daniel J. Schneider <dan@schneidan.com>
2018-04-18 15:14:23 -0600

    Move fclose, remove var_dump

--- subscribe-article.php
+++ subscribe-article.php
@@ -11,11 +11,10 @@ $date = date("D M d, Y G:i");
 
 if ($message_version) {
 	$line = array($date,$message_version,$source_url);
-	var_dump($line);
 	if (($handle = fopen("track-subscribe-article.csv", "a")) !== FALSE) {
 		fputcsv($handle, $line);
+		fclose($handle);
 	}
-	fclose($handle);
 }
 
 header("Location: http://dpo.st/subscribe-article");
